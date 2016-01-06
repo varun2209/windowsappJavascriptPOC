23f6194839dffceb58fff9617ed0a55ae4e2f566
varun2209 <varunsharma.dtu@gmail.com>
2016-01-06 18:29:17 +0530

    Changes for code control

---
+++ RuntimeComponent1/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuntimeComponent1
+{
+    public sealed class Employee
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public Employee()
+        {
+            firstName = "Varun";
+            lastName = "Sharma";
+        }
+
+        public string EmployeeName()
+        {
+            return this.firstName + " " + this.lastName;
+        }
+    }
+}
